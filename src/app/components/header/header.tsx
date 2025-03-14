0fcfc8a9cfac35e91e331e100b6d6ef9a5fc8f7e
gpng <geraldpng@gmail.com>
2025-03-14 18:04:48 +0800

    chore: update all PAP incumbents

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button } from '@/app/components/ui/button';
+
+const Header = () => {
+  return (
+    <div className="fixed right-0 top-0 p-2 flex">
+      <Button>Voter Resources</Button>
+    </div>
+  );
+};
+
+export default Header;
