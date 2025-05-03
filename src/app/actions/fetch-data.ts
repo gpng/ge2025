b6e1132d162f488c9b4da37ed632d6e69fad7632
gpng <geraldpng@gmail.com>
2025-05-03 22:49:17 +0800

    news: more sample

--- src/app/actions/fetch-data.ts
+++ src/app/actions/fetch-data.ts
@@ -5,6 +5,7 @@ import { electoralDivisionsSchema } from '@/models/electoral-division';
 import { newsSchema } from '@/models/news';
 import { partiesSchema } from '@/models/party';
 import { partyProfileSchema } from '@/models/profile';
+import electoralDivisions from '@data/electoral-divisions.json';
 import { unstable_cache } from 'next/cache';
 
 const CACHE_TTL = 600; // 10 minutes
@@ -17,7 +18,7 @@ const fetchDataWithCache = unstable_cache(
       const [parties, news, ed, profiles, boundaries] = await Promise.all([
         fetch(`${BASE_URL}/parties.json`).then((r) => r.json()),
         fetch(`${BASE_URL}/news.json`).then((r) => r.json()),
-        fetch(`${BASE_URL}/electoral-divisions.json`).then((r) => r.json()),
+        electoralDivisions,
         fetch(`${BASE_URL}/profiles.json`).then((r) => r.json()),
         fetch(`${BASE_URL}/boundaries.json`).then((r) => r.json()),
       ]);
